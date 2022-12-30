80e0ce6270105cf9b79d19eb6917f6acaa896b5f
L <luenwarneke@gmail.com>
2022-12-31 08:56:13 +1000

    .

--- index.js
+++ index.js
@@ -1,72 +1,77 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const url = 'https://sherrilltree.com/samson-stable-braid-rigging-rope-1-2in/';
+  try {
+    const url = 'https://sherrilltree.com/samson-stable-braid-rigging-rope-1-2in/';
 
-  const selectors = {
-    yotpo: 'div.yotpo.yotpo-main-widget',
-    reviews: 'div.yotpo-reviews',
-    review: 'div.yotpo-review',
-    name: 'span.yotpo-user-name',
-    rating: 'div.yotpo-review-stars span.sr-only',
-    title: 'div.yotpo-main div.content-title',
-    desc: 'div.content-review',
-    date: 'span.yotpo-review-date',
-    pager: 'div.yotpo-pager[data-total]',
-    next: 'div.yotpo-pager a[rel^=next]',
-  };
+    const selectors = {
+      yotpo: 'div.yotpo.yotpo-main-widget',
+      reviews: 'div.yotpo-reviews',
+      review: 'div.yotpo-review',
+      name: 'span.yotpo-user-name',
+      rating: 'div.yotpo-review-stars span.sr-only',
+      title: 'div.yotpo-main div.content-title',
+      desc: 'div.content-review',
+      date: 'span.yotpo-review-date',
+      pager: 'div.yotpo-pager[data-total]',
+      next: 'div.yotpo-pager a[rel^=next]',
+    };
 
-  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-  await page.setViewport({
-    width: 1280,
-    height: 1024,
-  });
-  await page.goto(url, { waitUntil: 'networkidle0' });
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1280,
+      height: 1024,
+    });
+    await page.goto(url, { waitUntil: 'networkidle0' });
 
-  await page.waitForSelector(selectors.reviews);
+    await page.waitForSelector(selectors.reviews);
 
-  // const html = await page.evaluate(selector => document.querySelector(selector.reviews).innerHTML, selectors);
+    // const html = await page.evaluate(selector => document.querySelector(selector.reviews).innerHTML, selectors);
 
-  const reviewsTotal = await page.evaluate(selector => document.querySelector(selector.pager).getAttribute('data-total'), selectors);
-  const reviewsPerPage = await page.evaluate(selector => document.querySelector(selector.pager).getAttribute('data-per-page'), selectors);
-  const reviewsPages = Math.ceil(reviewsTotal / reviewsPerPage);
-  console.log('Total reviews:', reviewsTotal);
-  console.log('Reviews per page:', reviewsPerPage);
-  console.log('Pages:', reviewsPages);
+    const reviewsTotal = await page.evaluate(selector => document.querySelector(selector.pager).getAttribute('data-total'), selectors);
+    const reviewsPerPage = await page.evaluate(selector => document.querySelector(selector.pager).getAttribute('data-per-page'), selectors);
+    const reviewsPages = Math.ceil(reviewsTotal / reviewsPerPage);
+    console.log('Total reviews:', reviewsTotal);
+    console.log('Reviews per page:', reviewsPerPage);
+    console.log('Pages:', reviewsPages);
 
-  var reviewsArr = [];
-  // while() { 'div.yotpo-pager a:not[.yotpo-disabled]'  }
-  for (let p = 1; p < reviewsPages + 1; p += 1) {
-    console.log('Getting page:', p);
-    const d = await page.evaluate((selector, p) => {
-      const reviews = document.querySelectorAll(selector.review);
-      let data = [];
-      for (let r = 0; r < reviews.length; r += 1) {
-        // const reviewNumber = data.length + 1;
-        data.push({
-          name: document.querySelectorAll(selector.name)[r].textContent.trim(),
-          rating: document.querySelectorAll(selector.rating)[r].textContent.trim(),
-          title: document.querySelectorAll(selector.title)[r].textContent.trim(),
-          desc: document.querySelectorAll(selector.desc)[r].textContent.trim(),
-          date: document.querySelectorAll(selector.date)[r].textContent.trim(),
-        });
+    let reviewsArr = [];
+    // while() { 'div.yotpo-pager a:not[.yotpo-disabled]'  }
+    for (let p = 1; p < reviewsPages + 1; p += 1) {
+      console.log('Getting page:', p);
+      const d = await page.evaluate((selector) => {
+        const reviews = document.querySelectorAll(selector.review);
+        const data = [];
+        for (let r = 0; r < reviews.length; r += 1) {
+          // const reviewNumber = data.length + 1;
+          data.push({
+            name: document.querySelectorAll(selector.name)[r].textContent.trim(),
+            rating: document.querySelectorAll(selector.rating)[r].textContent.trim(),
+            title: document.querySelectorAll(selector.title)[r].textContent.trim(),
+            desc: document.querySelectorAll(selector.desc)[r].textContent.trim(),
+            date: document.querySelectorAll(selector.date)[r].textContent.trim(),
+          });
+        }
+        return data;
+      }, selectors);
+
+      reviewsArr = [...reviewsArr, ...d];
+
+      // if not last page in pagination
+      if (!reviewsPages.length + 1 === p) {
+        // await page.waitFor(2000);
+
+        await page.click(selectors.next);
+        await page.waitForSelector(selectors.name);
       }
-      return data;
-    }, selectors, p);
-
-    reviewsArr = [...reviewsArr, ...d];
-
-    // if not last page in pagination
-    if (!reviewsPages.length + 1 === p) {
-      // await page.waitFor(2000);
-
-      await page.click(selectors.next);
-      await page.waitForSelector(selectors.name);
     }
+
+    console.log(reviewsArr);
+    console.log('Got', reviewsArr.length, 'reviews');
+
+    await browser.close();
+  } catch (error) {
+    console.log(`Error: ${error}`);
   }
-
-  console.log('Got', reviewsArr.length, 'reviews');
-
-  await browser.close();
 })();
